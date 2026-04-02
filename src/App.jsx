df3a146c14965b54706f3970e80dd7b85969158b
Maria Usina <usinamaria2189@gmail.com>
2026-04-02 13:59:31 -0500

    feat(compnents): creating new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
+import TodoForm from './TodoForm.jsx';
+import TodoList from './TodoList.jsx';
 
 function App() {
 
-  const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-]
-
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <ul>
-            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
-    </div>
-  )
+return (
+  <div>
+    <h1>Todo List</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
+);
 }
 
 export default App
