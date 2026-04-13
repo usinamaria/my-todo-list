ad5634b8c25c1cbfde141d399d189c3a60ae92cf
Maria Usina <usinamaria2189@gmail.com>
2026-04-13 16:01:49 -0500

    refactor(my-todo-list): Extract TodoListItem component and implement state management

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,10 @@
-function TodoList() {
-  const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
+import TodoListItem from './TodoListItem.jsx';
 
+function TodoList({todoList}) {
   return (
     <>
       <ul>
-        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
       </ul>
     </>
   )
