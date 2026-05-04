55fd9c90263caff1d409faa270532af490ad603e
Maria Usina <usinamaria2189@gmail.com>
2026-05-04 14:56:30 -0500

    Implement todo completion and filter logic
    - Add completeTodo function to handle marking todos as complete
    - Pass onCompleteTodo callback through TodoList to TodoListItem
    - Convert TodoForm to controlled component with workingTodoTitle state
    - Filter completed todos from display in TodoList
    - Add onChange handler to input for state-driven form

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,7 +1,14 @@
-function TodoListItem({todo}) {
-  return (
-    <li>{todo.title}</li>
-  )
+function TodoListItem({todo, onCompleteTodo}) {
+return (
+  <li>
+      <input
+        type="checkbox"
+        checked={todo.isCompleted}
+        onChange={() => onCompleteTodo(todo.id)}
+      />
+      {todo.title}
+  </li>
+)
 }
 
 export default TodoListItem
